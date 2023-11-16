91886caec92c7eb214c0194c013ea0287d362526
RaiIsono0914 <lairin0602@gmail.com>
2023-11-16 10:44:20 +0900

    test

---
+++ MyGitproject2d/src/main/java/com/example/demo/MyGitproject2dApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitproject2dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGitproject2dApplication.class, args);
+	}
+
+}
---
+++ MyGitproject2d/src/test/java/com/example/demo/MyGitproject2dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGitproject2dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
