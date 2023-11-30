d8add30e0eb0a82b544d4b40f7ea6243afc98d0d
S0518 <S0518@21S0518>
2023-11-30 09:53:57 +0900

    Merge branch 'master' of https://github.com/RaiIsono0914/2d_framework_name.git

--- MyGitproject2d/src/main/java/com/example/demo/controller/MergeHellController.java
+++ MyGitproject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -1,23 +1,15 @@
 package com.example.demo.controller;
 
-import java.util.Random;
-
 public class MergeHellController {
 	public static void main(String[] args) {
-//		kadaiA();
+		kadaiA();
 		kadaiB();
+
 	}
 
-
-    static int kadaiB() {
-        // ランダムな数値を生成
-        Random random = new Random();
-        int randomNumber = random.nextInt(100) + 1;
-
-        // 生成した数値を出力
-        System.out.println("生成されたランダムな数値: " + randomNumber);
-
-        // 数値を返す場合は、メソッドの戻り値として設定することもできます
-         return randomNumber;
-    }
+	static void kadaiA() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("現在" + i +"の何回目ループです。 ");
+		}
+	}
 }
