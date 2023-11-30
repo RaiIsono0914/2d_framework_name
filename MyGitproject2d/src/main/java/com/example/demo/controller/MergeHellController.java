d0685fa9033a8b07057b03b6e969dc4566287f49
RaiIsono0914 <lairin0602@gmail.com>
2023-11-30 09:56:42 +0900

    b

--- MyGitproject2d/src/main/java/com/example/demo/controller/MergeHellController.java
+++ MyGitproject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Random;
+
 public class MergeHellController {
 	public static void main(String[] args) {
 		kadaiA();
@@ -12,4 +14,17 @@ public class MergeHellController {
 			System.out.println("現在" + i +"の何回目ループです。 ");
 		}
 	}
+
+
+    static void kadaiB() {
+        // ランダムな数値を生成
+        Random random = new Random();
+        int randomNumber = random.nextInt(100) + 1;
+
+        // 生成した数値を出力
+        System.out.println("生成されたランダムな数値: " + randomNumber);
+
+        // 数値を返す場合は、メソッドの戻り値として設定することもできます
+        // return randomNumber;
+    }
 }
